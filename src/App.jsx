a95d15753620ffcfac5746df75fce04d076f7cb8
DharaneeswarDharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-03-25 18:20:18 +0530

    Dhanu

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">
+      Tailwind is Working!
+    </h1>
+  );
+};
+
+export default App;
